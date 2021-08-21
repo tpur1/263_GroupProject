faba0e2501939bd2ab397bf1239060baba304831
tpur1 <80999052+tpur1@users.noreply.github.com>
2021-08-21 15:17:12 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import numpy as np
+import scipy as sc
 from matplotlib import pyplot as plt
+from solve_ode import get_nitrate_concentration, ode_model, get_n_stock
 
 if __name__ == "__main__":
-    # 1. Get the nitrate concentration data 
+    # 1. Get the nitrate concentration data
+    t = ? #array of times used to find desired solution
+    C = ?
+    P = ?
+    m_0 = ?
+    t_c = ?
+    P_surface = ?
+    P_a = ?
+    P_mar = ?
+
+    t_calibrate, nitrate_calibrate = np.genfromtxt("data/nl_cows.txt", delimiter=",", skip_header=1)
     # 2. Get optimal parameters using curve_fit()
+    
+    n_stock = get_n_stock(t_calibrate)
+    #get stock numbers for t_calibrate
+    
+    paras = sc.optimize.curve_fit(ode_model, xdata = t_calibrate, ydata = nitrate_calibrate,p0 = ?)
+    [b_1, b_2, b_3, tau, alpha] = paras
+    #? is intial guess
+    
     # 3. Solve ODE numerically using optimal parameters
+    nitrate_conc_num = get_nitrate_concentration(t, b_1, b_2, b_3, tau, alpha)
+    
     # 4. Plot numeric solution and actual data
-    pass
+
