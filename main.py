3852edf0e7d81da6c6039742b1905f3c6d8cff07
AurelianoAureliano <80793406+rkam446@users.noreply.github.com>
2021-08-21 14:30:18 +1200

    wrote comments

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
+    # 1. Get the nitrate concentration data 
+    # 2. Get optimal parameters using curve_fit()
+    # 3. Solve ODE numerically using optimal parameters
+    # 4. Plot numeric solution and actual data
     pass
